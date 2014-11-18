5e5bbb51737f75e836f941afd740c2289dd51070
Oscar Velásquez <Javier Urresty>
2014-11-18 16:47:23 -0500

    Splash de la aplicación corregido

--- src/com/example/markapp/SplashScreenActivity.java
+++ src/com/example/markapp/SplashScreenActivity.java
@@ -2,47 +2,47 @@ package com.example.markapp;
 
 import java.util.Timer;
 import java.util.TimerTask;
- 
+  
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
- 
-public class SplashScreenActivity extends Activity {
- 
-    // Set the duration of the splash screen
+
+public class SplashScreenActivity extends Activity{
+
+	// Definimos la duración del splash screen
     private static final long SPLASH_SCREEN_DELAY = 3000;
- 
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
- 
-        // Set portrait orientation
+  
+        // Definimos la orientacion a vertical
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        // Hide title bar
+        // Escondemos la barra de Titulo
         requestWindowFeature(Window.FEATURE_NO_TITLE);
- 
-        setContentView(R.layout.splash_screen);
- 
+  
+        setContentView(R.layout.activity_splashscreen);
+  
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
- 
-                // Start the next activity
+  
+                // Llamamos a la Actividad principal de la aplicacion
                 Intent mainIntent = new Intent().setClass(
-                        SplashScreenActivity.this, MainActivity.class);
+                		SplashScreenActivity.this, MainActivity.class);
                 startActivity(mainIntent);
- 
-                // Close the activity so the user won't able to go back this
-                // activity pressing Back button
+  
+                // Cerramos la actividad para que el usuario no pueda 
+                // volver a ella presionando el boton Atras
                 finish();
             }
         };
- 
-        // Simulate a long loading process on application startup.
+  
+        // Simulación de un proceso de carga largo al iniciar la aplicación.
         Timer timer = new Timer();
         timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
- 
+
 }
